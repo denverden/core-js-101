43784909018fb5ff45cb88f8449e307b8751dc97
DenverDen <denverden@mail.ru>
2024-08-28 13:36:45 +0300

    feat: added solution toCsvText

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -171,8 +171,8 @@ function getHead(arr, n) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return arr.slice(arr.length - n, arr.length);
 }
 
 /**
@@ -195,8 +195,8 @@ function getTail(/* arr, n */) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  return arr.reduce((sum, value) => `${sum}\n${value.join(',')}`);
 }
 
 /**
